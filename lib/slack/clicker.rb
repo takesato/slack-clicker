0d07b8037b1521623ad7eb7967d7e1f246ef8045
takehito sato <takehitosato@gmail.com>
2016-08-14 15:30:14 +0900

    bundle gem slack-clicker

---
+++ lib/slack/clicker.rb
@@ -0,0 +1,7 @@
+require "slack/clicker/version"
+
+module Slack
+  module Clicker
+    # Your code goes here...
+  end
+end
---
+++ lib/slack/clicker/version.rb
@@ -0,0 +1,5 @@
+module Slack
+  module Clicker
+    VERSION = "0.1.0"
+  end
+end
