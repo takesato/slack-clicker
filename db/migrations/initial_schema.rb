b39d1a6195f9e220a22e35cd0624de4043c10eec
takehito sato <takehitosato@gmail.com>
2016-08-14 16:28:08 +0900

    first migration and model

---
+++ db/migrations/initial_schema.rb
@@ -0,0 +1,12 @@
+class InitialSchema < ActiveRecord::Migration
+  def self.up
+    create_table :channels do |t|
+      t.string :channel_id
+      t.string :count
+    end
+  end
+
+  def self.down
+    drop_table :channels
+  end
+end
--- lib/slack/clicker.rb
+++ lib/slack/clicker.rb
@@ -8,3 +8,4 @@ Dotenv.load
 require 'slack/clicker/version'
 require 'slack/clicker/config'
 require 'slack/clicker/client'
+Dir["#{File.dirname(__FILE__)}/clicker/models/**/*.rb"].each {|file| load file }
